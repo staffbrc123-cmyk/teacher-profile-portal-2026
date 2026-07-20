b0a340c4408a0eab228e65e8c4caa3e3bb4435a7
staffbrc123-cmyk <staffbrc123@gmail.com>
2026-07-20 10:22:59 +0530

    Add files via upload

--- subjects.js
+++ subjects.js
@@ -1,1 +1,25 @@
+const subjects = [
+  "भाषा (हिन्दी) अध्यापक ",
+  "गणित/विज्ञान अध्यापक ",
+  "सामाजिक विषय अध्यापक ",
+  "उर्दू अध्यापक ",
+  "भाषा (अंग्रेजी) अध्यापक ",
+  "खेल अनुदेशक ",
+  "कला अनुदेशक ",
+  "कंप्युटर अनुदेशक ",
+  "शिक्षामित्र (सभी विषय)",
+    "प्राथमिक शिक्षक (सभी विषय)",
+  ];
 
+function loadSubjects() {
+    const select = document.getElementById("subject");
+
+    subjects.forEach(subject => {
+        const option = document.createElement("option");
+        option.value = subject;
+        option.textContent = subject;
+        select.appendChild(option);
+    });
+}
+
+window.addEventListener("DOMContentLoaded", loadSubjects);
